99eef78d7c723bb4c39d1ba3f1f2033f64a20f05
dance2die <dontbothermewithspam@gmail.com>
2015-06-24 22:29:50 -0400

    Database not yet configured...

---
+++ CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CountingKs.Data;
+
+namespace CountingKs.Controllers
+{
+	public class FoodsController : ApiController
+	{
+		public object Get()
+		{
+			var repo = new CountingKsRepository(new CountingKsContext());
+			var result = repo.GetAllFoods()
+				.OrderBy(food => food.Description)
+				.Take(25)
+				.ToList();
+
+			return result;
+		}
+	}
+}
