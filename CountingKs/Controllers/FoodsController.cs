7e5067e84bdb67f86d890486b569c2c8ad5e5954
dance2die <dontbothermewithspam@gmail.com>
2015-06-27 21:38:51 -0400

    Returns "IEnumerable<Food>"

--- CountingKs/Controllers/FoodsController.cs
+++ CountingKs/Controllers/FoodsController.cs
@@ -1,12 +1,14 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using CountingKs.Data;
+using CountingKs.Data.Entities;
 
 namespace CountingKs.Controllers
 {
 	public class FoodsController : ApiController
 	{
-		public object Get()
+		public IEnumerable<Food> Get()
 		{
 			var repo = new CountingKsRepository(new CountingKsContext());
 			var result = repo.GetAllFoods()
