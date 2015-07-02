a06c885b702ec34abe744d97396288f6c55e1b67
dance2die <dontbothermewithspam@gmail.com>
2015-07-01 21:51:07 -0400

    Piggybagging on ASP.NET Authentication

--- CountingKs/Controllers/DiariesController.cs
+++ CountingKs/Controllers/DiariesController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Http;
 using CountingKs.Data;
 using CountingKs.Models;
 using CountingKs.Services;
 
 namespace CountingKs.Controllers
 {
+	[Authorize]
 	public class DiariesController : BaseApiController
 	{
 		private readonly ICountingKsIdentityService _identityService;
--- CountingKs/Services/CountingKsIdentityService.cs
+++ CountingKs/Services/CountingKsIdentityService.cs
@@ -1,7 +1,16 @@
-﻿namespace CountingKs.Services
+﻿using System.Threading;
+
+namespace CountingKs.Services
 {
 	public class CountingKsIdentityService : ICountingKsIdentityService
 	{
-		public string CurrentUser { get { return "shawnwildermuth"; }}
+		public string CurrentUser
+		{
+			get
+			{
+				//return "shawnwildermuth";
+				return Thread.CurrentPrincipal.Identity.Name;
+			}
+		}
 	}
 }
