8a05bb87eb13cf43976d968f1d7f855c4812c810
dance2die <dontbothermewithspam@gmail.com>
2015-07-01 21:00:31 -0400

    Upgraded MVC 4 & Web API to MVC5 & Web API 2
    http://www.asp.net/mvc/overview/releases/how-to-upgrade-an-aspnet-mvc-4-and-web-api-project-to-aspnet-mvc-5-and-web-api-2
    
    http://stackoverflow.com/questions/20396195/add-controller-option-removed-after-upgrading-mvc4-to-mvc5

--- CountingKs/Global.asax.cs
+++ CountingKs/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,20 +6,21 @@ using System.Web.Routing;
 
 namespace CountingKs
 {
-  // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-  // visit http://go.microsoft.com/?LinkId=9394801
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
 
-  public class MvcApplication : System.Web.HttpApplication
-  {
-    protected void Application_Start()
-    {
-      AreaRegistration.RegisterAllAreas();
+	public class MvcApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
 
-      WebApiConfig.Register(GlobalConfiguration.Configuration);
-      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-      RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);
-      AuthConfig.RegisterAuth();
-    }
-  }
+			//WebApiConfig.Register(GlobalConfiguration.Configuration);
+			WebApiConfig.Register(GlobalConfiguration.Configuration);
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			AuthConfig.RegisterAuth();
+		}
+	}
 }
